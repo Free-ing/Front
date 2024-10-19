db5ab456b52d43e488a8edfa58d87dd4eac29ad2
basilemon <joyejin1109@gachon.ac.kr>
2024-10-20 02:28:21 +0900

    수지가 말한 수정사항, 루틴 추가 페이지 진행 중

--- lib/screen/routine/add_routine_screen.dart
+++ lib/screen/routine/add_routine_screen.dart
@@ -101,7 +101,7 @@ class _AddRoutineScreenState extends State<AddRoutineScreen> {
                     SizedBox(width: screenWidth * 0.02),
                     Text(' 시간 선택', style: textTheme.bodyMedium),
                     SizedBox(width: screenWidth * 0.10),
-                    Text('시작: 8:00, 종료: 12:00',
+                    Text('시작: ${_startTime?.hour}:${_startTime?.minute}, 종료: 미설정}',
                         style:
                             textTheme.bodySmall!.copyWith(color: TEXT_PURPLE)),
                     IconButton(
