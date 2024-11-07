2016c14045373d0fb2a034f1b9875888506fa311
basilemon <joyejin1109@gachon.ac.kr>
2024-11-07 11:35:59 +0900

    수면 루틴 화면 크기 조금 늘렷엉

--- lib/screen/routine/sleep_tabbar_view.dart
+++ lib/screen/routine/sleep_tabbar_view.dart
@@ -116,7 +116,7 @@ class SleepTabBarViewState extends State<SleepTabBarView> {
           ],
         ),
         Container(
-          height: screenHeight * 0.5,
+          height: screenHeight * 0.53,
           child: GridView.builder(
             gridDelegate:
                 SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
