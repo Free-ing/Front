e1d78c55006eefacdc6abac87dfe8adc455dc008
SUJI LEE <soojilee3579@naver.com>
2024-11-13 21:21:52 +0900

    스플래시 스크린1?!?

--- lib/screen/member/splash_screen.dart
+++ lib/screen/member/splash_screen.dart
@@ -1,7 +1,90 @@
+// import 'package:flutter/material.dart';
+// import 'package:freeing/common/component/loading.dart';
+// import 'package:freeing/screen/member/login.dart';
+// import 'package:video_player/video_player.dart';
+//
+// class SplashScreen extends StatefulWidget {
+//   const SplashScreen({super.key});
+//
+//   @override
+//   State<SplashScreen> createState() => _SplashScreenState();
+// }
+//
+// class _SplashScreenState extends State<SplashScreen> {
+//   late VideoPlayerController _controller;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     // VideoPlayerController 초기화
+//     _controller = VideoPlayerController.asset('assets/imgs/login/splash.mp4')
+//       ..initialize().then((_) {
+//         setState(() {});
+//         _controller.play();
+//       });
+//
+//     // 영상이 끝나면 로그인 화면으로 이동
+//     _controller.addListener(() {
+//       if (_controller.value.position == _controller.value.duration) {
+//         Navigator.pushReplacement(
+//             context, MaterialPageRoute(builder: (context) => const Login()));
+//       }
+//     });
+//   }
+//
+//   @override
+//   void dispose() {
+//     _controller.dispose();
+//     super.dispose();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: _controller.value.isInitialized
+//             ? Stack(
+//           children: [
+//             Positioned.fill(
+//               child: FittedBox(
+//                 fit: BoxFit.cover, // 화면을 꽉 채우도록 설정
+//                 child: SizedBox(
+//                   width: _controller.value.size.width,
+//                   height: _controller.value.size.height,
+//                   child: VideoPlayer(_controller),
+//                 ),
+//               ),
+//             ),
+//           ],
+//         )
+//             : const Loading(),
+//
+//         // child: _controller.value.isInitialized
+//         //     ? FittedBox(
+//         //   fit: BoxFit.cover, // Ensures the video covers the screen
+//         //   child: SizedBox(
+//         //     width: _controller.value.size.width,
+//         //     height: _controller.value.size.height,
+//         //     child: VideoPlayer(_controller),
+//         //   ),
+//         // )
+//         //     : const CustomCircularProgressIndicator(),
+//
+//         // child: _controller.value.isInitialized
+//         //     ? AspectRatio(
+//         //         aspectRatio: _controller.value.aspectRatio,
+//         //         child: VideoPlayer(_controller),
+//         //       )
+//         //     : const CustomCircularProgressIndicator(),
+//       ),
+//     );
+//   }
+// }
+
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:freeing/common/component/loading.dart';
 import 'package:freeing/screen/member/login.dart';
-import 'package:video_player/video_player.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -11,72 +94,77 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  late VideoPlayerController _controller;
+  final List<String> images = [
+    'assets/imgs/etc/splash_1.png',
+    'assets/imgs/etc/splash_2.png',
+    'assets/imgs/etc/splash_3.png',
+    'assets/imgs/etc/splash_4.png',
+    'assets/imgs/etc/splash_5.png',
+    'assets/imgs/etc/splash_6.png',
+    'assets/imgs/etc/splash_7.png',
+    'assets/imgs/etc/splash_8.png',
+  ];
+
+  int currentImageIndex = 0;
+  Timer? _timer;
+  final int imageDisplayDuration = 500; // 1 second per image
 
   @override
   void initState() {
     super.initState();
-    // VideoPlayerController 초기화
-    _controller = VideoPlayerController.asset('assets/imgs/login/splash.mp4')
-      ..initialize().then((_) {
-        setState(() {});
-        _controller.play();
-      });
 
-    // 영상이 끝나면 로그인 화면으로 이동
-    _controller.addListener(() {
-      if (_controller.value.position == _controller.value.duration) {
-        Navigator.pushReplacement(
-            context, MaterialPageRoute(builder: (context) => const Login()));
-      }
-    });
+    _timer = Timer.periodic(
+      Duration(milliseconds: imageDisplayDuration),
+          (timer) {
+        if (currentImageIndex < images.length - 1) {
+          setState(() {
+            currentImageIndex++;
+          });
+        } else {
+          // Navigate to Login screen when the last image finishes
+          _timer?.cancel();
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => const Login()),
+
+          );
+        }
+      },
+    );
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    _timer?.cancel();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: _controller.value.isInitialized
-            ? Stack(
-          children: [
-            Positioned.fill(
-              child: FittedBox(
-                fit: BoxFit.cover, // 화면을 꽉 채우도록 설정
-                child: SizedBox(
-                  width: _controller.value.size.width,
-                  height: _controller.value.size.height,
-                  child: VideoPlayer(_controller),
-                ),
-              ),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              images[currentImageIndex],
+              fit: BoxFit.cover,
             ),
-          ],
-        )
-            : const Loading(),
-
-        // child: _controller.value.isInitialized
-        //     ? FittedBox(
-        //   fit: BoxFit.cover, // Ensures the video covers the screen
-        //   child: SizedBox(
-        //     width: _controller.value.size.width,
-        //     height: _controller.value.size.height,
-        //     child: VideoPlayer(_controller),
-        //   ),
-        // )
-        //     : const CustomCircularProgressIndicator(),
-
-        // child: _controller.value.isInitialized
-        //     ? AspectRatio(
-        //         aspectRatio: _controller.value.aspectRatio,
-        //         child: VideoPlayer(_controller),
-        //       )
-        //     : const CustomCircularProgressIndicator(),
+          ),
+          // Positioned(
+          //   bottom: 20.0,
+          //   right: 20.0,
+          //   child: Text(
+          //     'Time Remaining: ${(images.length - currentImageIndex - 1) * imageDisplayDuration ~/ 1000}s',
+          //     style: const TextStyle(
+          //       fontSize: 16,
+          //       fontWeight: FontWeight.bold,
+          //       color: Colors.white,
+          //     ),
+          //   ),
+          // ),
+        ],
       ),
     );
   }
 }
+
